24721b246b9d8b73dd9191fe4dd0526cbe99c68e
salowed <salowed@gmail.com>
2014-05-01 16:19:15 -0400

    Added calendar and graph structure

--- LifeTracker/DSSelectedActitvityViewController.h
+++ LifeTracker/DSSelectedActitvityViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface DSSelectedActitvityViewController : UIViewController
+@interface DSSelectedActitvityViewController : UIViewController<CPTBarPlotDataSource, CPTBarPlotDelegate>
 @property (strong, nonatomic) NSString *activityTitle;
 
 @end
